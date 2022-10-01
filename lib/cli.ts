74a8dc5cea56896122d9e23b1faf3f7bcb802215
Gerald <gera2ld@live.com>
2022-10-01 15:40:11 +0800

    fix: read all data from stdout and stderr

--- lib/cli.ts
+++ lib/cli.ts
@@ -1,17 +1,30 @@
-export { colors } from "./deps/deno.ts";
+import { readAll } from "./deps/deno.ts";
 
-export async function runCommand(
-  cmd: string[],
-  opts?: Omit<Deno.RunOptions, "cmd">,
-) {
-  const p = Deno.run({ ...opts, cmd });
+export { colors, readAll } from "./deps/deno.ts";
+
+export async function runCommand(options: Deno.RunOptions) {
+  const p = Deno.run(options);
   const status = await p.status();
   if (!status.success) throw new Error(`Exit code: ${status.code}`);
   return p;
 }
 
 export async function evalCommand(cmd: string[]) {
-  const p = await runCommand(cmd, { stdout: "piped" });
-  const output = await p.output();
-  return new TextDecoder().decode(output);
+  const p = Deno.run({
+    cmd,
+    stdout: "piped",
+    stderr: "piped",
+  });
+  // Pipe buffer is limited to 64K, so we must readAll from the stream to get complete data
+  const [status, stdout, stderr] = await Promise.all([
+    p.status(),
+    p.stdout && readAll(p.stdout),
+    p.stderr && readAll(p.stderr),
+  ]);
+  if (!status.success) throw new Error(`Exit code: ${status.code}`);
+  const textDecoder = new TextDecoder();
+  return {
+    stdout: stdout && textDecoder.decode(stdout),
+    stderr: stderr && textDecoder.decode(stderr),
+  };
 }
