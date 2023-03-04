04d2255df77c93920664f3b0c24488de34c1cd32
Gerald <code@gera2ld.space>
2023-03-04 18:53:25 +0800

    feat: add reqinfo

---
+++ lib/reqinfo.ts
@@ -0,0 +1,18 @@
+import { serve } from "./deps/deno.ts";
+
+function handleRequest(request: Request) {
+  const output = {
+    method: request.method,
+    url: request.url,
+    headers: Array.from(request.headers.entries()),
+  };
+  return new Response(JSON.stringify(output), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+await serve(handleRequest, {
+  port: 8080,
+});
