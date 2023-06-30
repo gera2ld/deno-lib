5be3148de3fc3b2da727bb35b26f29da84c5b4d7
Gerald <code@gera2ld.space>
2023-06-30 22:50:36 +0800

    fix: update cli utils

--- lib/cli.ts
+++ lib/cli.ts
@@ -1,30 +1,26 @@
-import { readAll } from "./deps/deno.ts";
-
 export { colors, readAll } from "./deps/deno.ts";
 
-export async function runCommand(options: Deno.RunOptions) {
-  const p = Deno.run(options);
-  const status = await p.status();
+export async function runCommand(command: string | URL, options: Deno.CommandOptions) {
+  const cmd = new Deno.Command(command, options);
+  const p = cmd.spawn();
+  const status = await p.status;
   if (!status.success) throw new Error(`Exit code: ${status.code}`);
   return p;
 }
 
-export async function evalCommand(cmd: string[]) {
-  const p = Deno.run({
-    cmd,
+export async function evalCommand(command: string | URL, options: Deno.CommandOptions) {
+  const cmd = new Deno.Command(command, {
+    ...options,
     stdout: "piped",
     stderr: "piped",
   });
-  // Pipe buffer is limited to 64K, so we must readAll from the stream to get complete data
-  const [status, stdout, stderr] = await Promise.all([
-    p.status(),
-    p.stdout && readAll(p.stdout),
-    p.stderr && readAll(p.stderr),
-  ]);
-  if (!status.success) throw new Error(`Exit code: ${status.code}`);
+  const p = cmd.spawn();
+  // FIXME: Pipe buffer is limited to 64K, so we must readAll from the stream to get complete data
+  const result = await p.output();
+  if (!result.success) throw new Error(`Exit code: ${result.code}`);
   const textDecoder = new TextDecoder();
   return {
-    stdout: stdout && textDecoder.decode(stdout),
-    stderr: stderr && textDecoder.decode(stderr),
+    stdout: result.stdout && textDecoder.decode(result.stdout),
+    stderr: result.stderr && textDecoder.decode(result.stderr),
   };
 }
