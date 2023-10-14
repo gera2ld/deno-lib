4fa022c70e4827f6fb81864c743093da0429de17
Gerald <code@gera2ld.space>
2023-10-14 11:31:27 +0800

    fix: update deps

--- lib/deps/deno.ts
+++ lib/deps/deno.ts
@@ -1,37 +1,37 @@
-export { ensureFile } from "https://deno.land/std@0.192.0/fs/mod.ts";
+export { ensureFile } from "https://deno.land/std@0.204.0/fs/mod.ts";
 export {
   basename,
   dirname,
   fromFileUrl,
   join,
   resolve,
-} from "https://deno.land/std@0.192.0/path/mod.ts";
+} from "https://deno.land/std@0.204.0/path/mod.ts";
 
-export * as base64 from "https://deno.land/std@0.192.0/encoding/base64.ts";
-export * as base64url from "https://deno.land/std@0.192.0/encoding/base64url.ts";
+export * as base64 from "https://deno.land/std@0.204.0/encoding/base64.ts";
+export * as base64url from "https://deno.land/std@0.204.0/encoding/base64url.ts";
 
 export {
   load,
   type LoadOptions,
   loadSync,
-} from "https://deno.land/std@0.192.0/dotenv/mod.ts";
+} from "https://deno.land/std@0.204.0/dotenv/mod.ts";
 
 export {
   getLogger,
   handlers,
   setup,
-} from "https://deno.land/std@0.192.0/log/mod.ts";
-export type { LevelName } from "https://deno.land/std@0.192.0/log/mod.ts";
+} from "https://deno.land/std@0.204.0/log/mod.ts";
+export type { LevelName } from "https://deno.land/std@0.204.0/log/mod.ts";
 
-export { parse } from "https://deno.land/std@0.192.0/flags/mod.ts";
+export { parse } from "https://deno.land/std@0.204.0/flags/mod.ts";
 
-export { format as formatTime } from "https://deno.land/std@0.192.0/datetime/mod.ts";
-export * as colors from "https://deno.land/std@0.192.0/fmt/colors.ts";
+export { format as formatTime } from "https://deno.land/std@0.204.0/datetime/mod.ts";
+export * as colors from "https://deno.land/std@0.204.0/fmt/colors.ts";
 
 export {
   readAll,
   readerFromStreamReader,
-} from "https://deno.land/std@0.192.0/streams/mod.ts";
+} from "https://deno.land/std@0.204.0/streams/mod.ts";
 
-export { serve } from "https://deno.land/std@0.192.0/http/server.ts";
-export type { ServeInit } from "https://deno.land/std@0.192.0/http/server.ts";
+export { serve } from "https://deno.land/std@0.204.0/http/server.ts";
+export type { ServeInit } from "https://deno.land/std@0.204.0/http/server.ts";
